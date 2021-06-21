e83a6802797b2521e82dc41df812c93d09ef0169
Arpit Upadhyay <arpit.upadhyay01@nagarro.com>
2021-06-21 18:46:10 +0530

    Typecasting Done

---
+++ src/app/iplData.ts
@@ -0,0 +1,39 @@
+export interface IData {
+    city: string;
+    date: string;
+    dl_applied: number;
+    id: number;
+    player_of_match: string;
+    result: string;
+    season: number;
+    team1: string;
+    team2: string;
+    toss_decision: string;
+    toss_winner: string;
+    umpire1: string;
+    umpire2: string;
+    umpire3: string;
+    venue: string;
+    win_by_runs: number;
+    win_by_wickets: number;
+    winner: string;
+}
+
+// city: "Bangalore"
+// date: "18-04-08"
+// dl_applied: 0
+// id: 1
+// player_of_match: "BB McCullum"
+// result: "normal"
+// season: 2008
+// team1: "Kolkata Knight Riders"
+// team2: "Royal Challengers Bangalore"
+// toss_decision: "field"
+// toss_winner: "Royal Challengers Bangalore"
+// umpire1: "Asad Rauf"
+// umpire2: "RE Koertzen"
+// umpire3: ""
+// venue: "M Chinnaswamy Stadium"
+// win_by_runs: 140
+// win_by_wickets: 0
+// winner: "Kolkata Knight Riders"
